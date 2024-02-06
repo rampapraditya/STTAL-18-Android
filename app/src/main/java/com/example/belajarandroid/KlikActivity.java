9afdbeaeffdbe6258d5bac06aece23cb31f0119e
rampapraditya <rampapraditya@gmail.com>
2024-02-06 12:46:48 +0700

    aa

--- app/src/main/java/com/example/belajarandroid/KlikActivity.java
+++ app/src/main/java/com/example/belajarandroid/KlikActivity.java
@@ -3,12 +3,24 @@ package com.example.belajarandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class KlikActivity extends AppCompatActivity {
 
+    Button btn1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_klik);
+
+        btn1 = findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(KlikActivity.this, "Tombol di klik", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
