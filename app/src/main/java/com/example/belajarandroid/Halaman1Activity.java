4ec8131ff21a1f56096c4358e208948bd1c18498
rampapraditya <rampapraditya@gmail.com>
2024-02-19 09:35:27 +0700

    lupa password

--- app/src/main/java/com/example/belajarandroid/Halaman1Activity.java
+++ app/src/main/java/com/example/belajarandroid/Halaman1Activity.java
@@ -6,10 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Halaman1Activity extends AppCompatActivity {
 
-    Button btnKlik;
+    Button btnKlik, btnKeluar;
+    TextView lupa;
+    EditText inputan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +21,31 @@ public class Halaman1Activity extends AppCompatActivity {
         setContentView(R.layout.activity_halaman1);
 
         btnKlik = findViewById(R.id.btnKlik);
+        inputan = findViewById(R.id.inputan);
+        btnKeluar = findViewById(R.id.btnKeluar);
+        lupa = findViewById(R.id.lupa);
+
         btnKlik.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // fungsinya pindah halaman
-                Intent intentKeHal2 = new Intent(Halaman1Activity.this,
-                        Halaman2Activity.class);
-                startActivity(intentKeHal2);
+                Intent hal2 = new Intent(Halaman1Activity.this, Halaman2Activity.class);
+                hal2.putExtra("AAA", inputan.getText().toString()); // lempar inputan ke halaman lain
+                startActivity(hal2);
+            }
+        });
+
+        btnKeluar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        lupa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
             }
         });
     }
--- app/src/main/java/com/example/belajarandroid/Halaman2Activity.java
+++ app/src/main/java/com/example/belajarandroid/Halaman2Activity.java
@@ -6,16 +6,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Halaman2Activity extends AppCompatActivity {
 
     Button btn2;
+    TextView input;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_halaman2);
 
         btn2 = findViewById(R.id.btn2);
+        input = findViewById(R.id.input);
+
+        // menerima inputan dari halaman lain
+        Intent nilaidari_halLain = getIntent();
+        input.setText(nilaidari_halLain.getStringExtra("AAA"));
+
+
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
