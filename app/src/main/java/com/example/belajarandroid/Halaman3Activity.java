bc63f50048e236d18fad319485d40f0e5d533ec3
rampapraditya <rampapraditya@gmail.com>
2024-02-19 09:19:42 +0700

    revisi

--- app/src/main/java/com/example/belajarandroid/Halaman3Activity.java
+++ app/src/main/java/com/example/belajarandroid/Halaman3Activity.java
@@ -2,26 +2,23 @@ package com.example.belajarandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class Halaman2Activity extends AppCompatActivity {
+public class Halaman3Activity extends AppCompatActivity {
 
     Button btn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_halaman2);
+        setContentView(R.layout.activity_halaman3);
 
         btn2 = findViewById(R.id.btn2);
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intentKeHal2 = new Intent(Halaman2Activity.this,
-                        Halaman3Activity.class);
-                startActivity(intentKeHal2);
+                finish();
             }
         });
     }
