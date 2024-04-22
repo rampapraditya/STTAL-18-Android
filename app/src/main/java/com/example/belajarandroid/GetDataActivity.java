0645b6c92f9221a8e34574d715cfb5d6dd41ac1f
rampapraditya <rampapraditya@gmail.com>
2024-04-22 09:35:39 +0700

    get data from url

--- app/src/main/java/com/example/belajarandroid/GetDataActivity.java
+++ app/src/main/java/com/example/belajarandroid/GetDataActivity.java
@@ -3,9 +3,17 @@ package com.example.belajarandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class GetDataActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -23,7 +31,29 @@ public class GetDataActivity extends AppCompatActivity implements View.OnClickLi
     @Override
     public void onClick(View v) {
         if(v == btnGetData){
-            Toast.makeText(this, "Sudah di klik", Toast.LENGTH_SHORT).show();
+            load_data("https://senjata.d3informatika-sttal.com/ws/loadpengajuan");
         }
+    }
+
+    private void load_data(String url) {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                response -> {
+                    try {
+                        Log.d("INFO", response);
+                    } catch (Exception e) {
+                        Toast.makeText(GetDataActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                },
+                error -> {
+                    Toast.makeText(GetDataActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+                }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.clear();
+                return params;
+            }
+        };
+        Volley.newRequestQueue(GetDataActivity.this).add(stringRequest);
     }
 }
